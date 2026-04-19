d14e04d6464767f53d7524f643d41348af863bec
zhehaoyi <wonderingpanda510@gmail.com>
2026-04-19 09:32:26 -0500

    update shell

--- scripts/main.sh
+++ scripts/main.sh
@@ -12,7 +12,7 @@ mkdir -p logs
 
 LOG_FILE="logs/train.log"
 
-echo "Running imagenet_impl.py" | tee -a "$LOG_FILE"
-python -u scripts/imagenet_impl.py \
+echo "Running main.py" | tee -a "$LOG_FILE"
+python -u scripts/main.py \
     2>&1 | tee -a "$LOG_FILE"
 echo "Finished everything" | tee -a "$LOG_FILE"
